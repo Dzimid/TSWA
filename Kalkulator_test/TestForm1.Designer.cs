80aa94a39be3365f7312c07f24bc05b783835d6b
mduliszewski <mduliszewski@gmail.com>
2017-11-15 20:26:35 +0100

    Form name change (#8)

---
+++ Kalkulator_test/TestForm1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Kalkulator_test
+{
+    partial class TestForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testResult
+            // 
+            this.testResult.Location = new System.Drawing.Point(13, 13);
+            this.testResult.Name = "testResult";
+            this.testResult.Size = new System.Drawing.Size(75, 23);
+            this.testResult.TabIndex = 0;
+            this.testResult.Text = "Test result";
+            this.testResult.UseVisualStyleBackColor = true;
+            this.testResult.Click += new System.EventHandler(this.testResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.testResult);
+            this.Name = "Form1";
+            this.Text = "Testy kalkulatora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testResult;
+    }
+}
+
---
+++ Kalkulator_test/TestForm1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kalkulator;
+
+namespace Kalkulator_test
+{
+    public partial class TestForm1 : Form
+    {
+        public TestForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void testResult_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var form = new Form1();
+            form.Show();
+
+            var calculator = new Calculator();
+
+            if (0 == calculator.result) {
+                testResult.ForeColor = Color.Green;
+            }
+        }
+    }
+}
