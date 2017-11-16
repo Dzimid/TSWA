25c4757cffb23eb3c119775025636063ecd9b8d1
Dzimi <jac3k.dziemidowicz@gmail.com>
2017-11-16 20:47:21 +0100

    Zmiana początkowego systemu loczbowego, drobne poprawki w octTable

--- Kalkulator/Calculator.cs
+++ Kalkulator/Calculator.cs
@@ -11,14 +11,14 @@ namespace Kalkulator
         private string result;
         private string systemV;
         private char[] binTable = { '0', '1' };
-        private char[] octTable = { '0', '1', '2', '3', '4', '5', '6', '7'};
+        private char[] octTable = { '0', '1', '2', '3', '4', '5', '6', '7' };
         private char[] decTable = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         private char[] hexTable = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
         public Calculator()
         {
             this.result = "0";
-            this.systemV = "HEX";
+            this.systemV = "DEC";
         }
 
         public string Result
